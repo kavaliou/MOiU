c67e1fed6984f2b08ecdadbe7fc5241917299ffb
Stanislau Kavaliou <kavalioustanislau@gmail.com>
2016-03-07 16:47:45 +0300

    Refactored first lab.

---
+++ examples.py
@@ -0,0 +1,8 @@
+from first import reversal_matrix
+import numpy as np
+
+C1 = np.array([1, 2, 2, 4, 1, 2, 4, 2, 3], dtype=np.float).reshape((3, 3))
+C2 = np.array([0, 2, 1, 0, 1, 1, 1, 1, 1], dtype=np.float).reshape((3, 3))
+
+print reversal_matrix(C1)
+print reversal_matrix(C2)
